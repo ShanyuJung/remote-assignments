9f136b6a545ae06ed29dd77ee4c1fe559891bf08
Luka <srps8912@gmail.com>
2022-08-17 19:25:20 +0800

    Independent function max()

--- week2/assignment1/app.js
+++ week2/assignment1/app.js
@@ -2,9 +2,26 @@ const button = document.getElementById("computeBtn");
 const input = document.getElementById("numsInput");
 const warning = document.getElementById("warningText");
 const result = document.getElementById("resultText");
-button.addEventListener("click", max);
+button.addEventListener("click", maxNumFunc);
 
 function max(numbers) {
+  let maxNum;
+  if (numbers.length == 1) {
+    maxNum = numbers[0];
+  } else {
+    maxNum = numbers[0];
+    for (let i = 1; i < numbers.length; i++) {
+      if (numbers[i] > maxNum) {
+        maxNum = numbers[i];
+      } else {
+        maxNum = maxNum;
+      }
+    }
+  }
+  return maxNum;
+}
+
+function maxNumFunc(numbers) {
   //format warning text and resultText
   warning.textContent = "";
   result.textContent = "";
@@ -29,18 +46,22 @@ function max(numbers) {
 
   //Find the max number of array by for loop
   let maxNum;
-  if (numArr.length == 1) {
-    maxNum = numArr[0];
-  } else {
-    maxNum = numArr[0];
-    for (let i = 1; i < numArr.length; i++) {
-      if (numArr[i] > maxNum) {
-        maxNum = numArr[i];
-      } else {
-        maxNum = maxNum;
-      }
-    }
-  }
+  maxNum = max(numArr);
+
+  // if (numArr.length == 1) {
+  //   maxNum = numArr[0];
+  // } else {
+  //   maxNum = numArr[0];
+  //   for (let i = 1; i < numArr.length; i++) {
+  //     if (numArr[i] > maxNum) {
+  //       maxNum = numArr[i];
+  //     } else {
+  //       maxNum = maxNum;
+  //     }
+  //   }
+  // }
+
+  // max(numArr);
   // console.log(maxNum)
   //Show the max number in HTML
   result.textContent = `The max number of array is ${maxNum}`;
